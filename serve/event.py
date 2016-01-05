eefb76d54d35e2aece0c4fadf8eb96cae3c966ac
Evan Duffield <evan@dashpay.io>
2016-01-05 14:53:13 -0700

    add missing event py

---
+++ serve/event.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+"""
+Dash-2T
+----
+
+Dash Event
+
+"""
+
+import argparse
+import zmq
+from websocket import create_connection
+
+parser = argparse.ArgumentParser(description='dash event script')
+parser.add_argument('--event')
+args = parser.parse_args()
+
+if args.event:
+    # with open(args.event, 'r') as myfile:
+    #     data=myfile.read().replace('\n', '')
+
+    ws = create_connection("ws://localhost:5000")
+    print args.event
+    ws.send(args.event)
+    result =  ws.recv()
+    print "Received '%s'" % result
+    ws.close()
