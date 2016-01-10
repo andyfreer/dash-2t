924abf376a45466d6f7de195659afa9334bbeadb
Evan Duffield <evan@dashpay.io>
2016-01-10 14:46:31 -0700

    bind to external address

--- websockets.py
+++ websockets.py
@@ -44,7 +44,7 @@ def message_received(client, server, message):
 
 
 PORT=5000
-server = WebsocketServer(PORT)
+server = WebsocketServer(host="0.0.0.0", port=PORT)
 server.set_fn_new_client(new_client)
 server.set_fn_client_left(client_left)
 server.set_fn_message_received(message_received)
