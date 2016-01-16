d63979dc778cf0653d25098579ce69044cb4f0cd
Evan Duffield <evan@dashpay.io>
2016-01-16 08:16:30 -0700

    allow cors

--- http.py
+++ http.py
@@ -5,8 +5,10 @@ from random import randint
 import websocket
 import threading
 from time import sleep
+from flask.ext.cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 dapi = None
 
 messages = []
